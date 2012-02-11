4b685e570de5c0f7f956ffa90d0a11245be2d20b
Gregory Liras <gregliras@gmail.com>
2012-02-11 20:14:34 +0200

    added libs control back to user

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -11,7 +11,7 @@ fpath=($ZSH/functions $ZSH/completions $fpath)
 
 # Load all of the config files in ~/oh-my-zsh that end in .zsh
 # TIP: Add files you don't want in git to .gitignore
-for config_file ($ZSH/lib/*.zsh) source $config_file
+for lib ($libs); source $ZSH/lib/$lib.zsh
 
 # Set ZSH_CUSTOM to the path where your custom config files
 # and plugins exists, or else we will use the default custom/
